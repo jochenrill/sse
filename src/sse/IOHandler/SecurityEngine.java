d16bb7366d88f37dbdf43dd76b18df8ec331cd66
jochen <jochen@jochen-netbook>
2011-01-26 01:51:31 +0100

    Finished SecurityEngine implementation.

--- src/sse/IOHandler/SecurityEngine.java
+++ src/sse/IOHandler/SecurityEngine.java
@@ -1,7 +1,5 @@
 package sse.IOHandler;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,108 +15,112 @@ import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 
 public class SecurityEngine {
+    private Key secureKey;
 
-	public static void decrypt(String fileName, String keyFile) {
+    public SecurityEngine() {
+        KeyGenerator kg;
+        try {
+            kg = KeyGenerator.getInstance("AES");
+            kg.init(new SecureRandom());
+            secureKey = kg.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-		try {
+    public void printKey(String fileName) {
+        BinaryOut keyStream;
+        try {
+            keyStream = new BinaryOut(fileName);
+            byte[] key = secureKey.getEncoded();
+            for (int i = 0; i < key.length; i++) {
+                keyStream.write(key[i]);
+            }
+            keyStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-			BinaryOut b = new BinaryOut(fileName + ".dec");
-			Cipher c = Cipher.getInstance("AES");
-			/*
-			 * KeyGenerator kg = KeyGenerator.getInstance("AES"); kg.init(new
-			 * SecureRandom());
-			 */
-			DataInputStream keyStream = new DataInputStream(
-					new DataInputStream(new FileInputStream(new File(keyFile))));
+    public void readKey(String fileName) {
+        DataInputStream keyStream;
+        try {
+            keyStream = new DataInputStream(new DataInputStream(
+                    new FileInputStream(new File(fileName))));
+            byte[] key = new byte[16];
+            keyStream.read(key, 0, 16);
+            keyStream.close();
+            secureKey = new SecretKeySpec(key, "AES");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-			int pos = 0;
-			byte[] key = new byte[keyStream.available()];
-			while (pos < keyStream.available()) {
-				key[pos] = (byte) keyStream.read();
-				pos++;
-			}
+    public void decrypt(String fileName) {
+        try {
+            FileOutputStream b = new FileOutputStream(fileName + ".dec");
+            Cipher c = Cipher.getInstance("AES");
+            c.init(Cipher.DECRYPT_MODE, secureKey);
+            CipherInputStream cis = new CipherInputStream(new FileInputStream(
+                    fileName + ".sec"), c);
+            byte[] block = new byte[8];
+            int i;
+            while ((i = cis.read(block)) != -1) {
+                b.write(block, 0, i);
+            }
+            cis.close();
+            b.close();
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-			keyStream.close();
-			Key k = new SecretKeySpec(key, "AES");
-			c.init(Cipher.DECRYPT_MODE, k);
-			CipherInputStream o = new CipherInputStream(new FileInputStream(
-					fileName + ".sec"), c);
-
-			pos = 0;
-			int in = 0;
-			while ((in = o.read()) != -1) {
-				b.write((byte) in);
-				pos++;
-			}
-			o.close();
-			b.close();
-
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	public static void encrypt(String fileName, String keyFile) {
-
-		try {
-
-			DataInputStream b = new DataInputStream(
-					new FileInputStream(fileName));
-			Cipher c = Cipher.getInstance("AES");
-			KeyGenerator kg = KeyGenerator.getInstance("AES");
-			kg.init(new SecureRandom());
-			BinaryOut keyStream = new BinaryOut(keyFile);
-
-			SecretKey k = kg.generateKey();
-
-			byte[] key = k.getEncoded();
-			for (int i = 0; i < key.length; i++) {
-				keyStream.write(key[i]);
-			}
-			keyStream.close();
-
-			c.init(Cipher.ENCRYPT_MODE, k);
-			CipherOutputStream o = new CipherOutputStream(new FileOutputStream(
-					fileName + ".sec"), c);
-
-			int pos = 0;
-			while (pos < b.available()) {
-				o.write(b.read());
-				pos++;
-			}
-			o.close();
-
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
+    public void encrypt(String fileName) {
+        try {
+            DataInputStream b = new DataInputStream(new FileInputStream(
+                    fileName));
+            Cipher c = Cipher.getInstance("AES");
+            
+            c.init(Cipher.ENCRYPT_MODE, secureKey);
+            CipherOutputStream cos = new CipherOutputStream(
+                    new FileOutputStream(fileName + ".sec"), c);
+            int i;
+            while ((i = b.read()) != -1) {
+                cos.write(i);
+            }
+            cos.close();
+            b.close();
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
--- src/sse/Main.java
+++ src/sse/Main.java
@@ -21,172 +21,178 @@ import sse.Vectors.OutOfMemoryVG;
 import sse.Vectors.SuffixVector;
 
 public class Main {
-	public static String text;
+    public static String text;
 
-	public static void main(String[] args) {
-		Options options = new Options();
-		options.addOption("i", true,
-				"Defines the input file. Define either an input file or a string.");
-		options.addOption("o", true,
-				"Defines the output file. \"default.vc\" if no file is given.");
-		options.addOption("s", true,
-				"Defines the String used for input. Define either a string or an input file.");
-		options.addOption("e", false,
-				"Runs an evaluation of the generated file");
-		options.addOption("m", false, "Generates the SuffixVectors in memory");
-		options.addOption("h", false, "Prints help");
-		options.addOption("help", false, "Prints this help message");
-		options.addOption("v", false, "Verbose");
-		options.addOption("b", false, "Split in blocks. Default turned off");
-		CommandLineParser parser = new GnuParser();
-		// Define a series of variables to check command line options
-		String outputFile = null;
-		boolean evaluate = false;
-		boolean blocks = false;
-		String input = null;
-		boolean inMemory = false;
-		boolean verbose = false;
-		try {
-			CommandLine cmd = parser.parse(options, args);
-			if (cmd.hasOption("i")) {
-				File inputFile = new File(cmd.getOptionValue("i"));
-				BufferedReader r = new BufferedReader(new FileReader(inputFile));
-				input = r.readLine();
-			} else if (cmd.hasOption("s")) {
-				input = cmd.getOptionValue("s");
-			} else {
-				throw new MissingOptionException(
-						"You must define an input, either a string or a file");
-			}
-			if (cmd.hasOption("o")) {
-				outputFile = cmd.getOptionValue("o");
-			} else {
-				outputFile = "default.vc";
-			}
-			if (cmd.hasOption("b")) {
-				blocks = true;
-			}
-			if (cmd.hasOption("e")) {
-				evaluate = true;
-			}
-			if (cmd.hasOption("m")) {
-				inMemory = true;
-			}
-			if (cmd.hasOption("v")) {
-				verbose = true;
-			}
-			if (cmd.hasOption("h") || cmd.hasOption("help")) {
-				HelpFormatter h = new HelpFormatter();
-				h.printHelp("Usage: main [options]", options);
-				System.exit(0);
-			}
-		} catch (ParseException e) {
-			System.out.println("Can't parse options: " + e.getMessage());
-			System.exit(0);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// s = "ananasan$";
-		// s = "aatttatttatta$";
-		double generalTime = System.currentTimeMillis();
-		double time = System.currentTimeMillis();
-		CDWAG t = new CDWAG(input);
-		if (verbose) {
-			System.out.println("Excecution time for generating the graph: "
-					+ ((System.currentTimeMillis() - time) / 1000));
-		}
-		time = System.currentTimeMillis();
-		ArrayList<SuffixVector> list = new ArrayList<SuffixVector>();
-		ArrayList<EdgePosition> ep = new ArrayList<EdgePosition>();
-		if (inMemory) {
-			InMemoryVG generator = new InMemoryVG(t);
-			list = generator.getListOfVectors();
-			ep = generator.getListOfEdges();
-			t = null;
-		} else {
-			OutOfMemoryVG generator = new OutOfMemoryVG(t);
-			File vectorFile = new File("_vector.tmp");
-			try {
-				generator.printListOfVectors(vectorFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			t = null;
-			try {
-				ObjectInputStream o = new ObjectInputStream(
-						new FileInputStream("_vector.tmp"));
-				Object obj = null;
-				while (!((obj = o.readObject()) instanceof String)) {
-					list.add((SuffixVector) obj);
-				}
-				o.close();
-				vectorFile.delete();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			for (SuffixVector v : list) {
-				for (EdgePosition e : v.getMap().values()) {
-					ep.add(e);
-				}
-			}
-		}
-		if (verbose) {
-			System.out.println("Excecution time for generating the vectors: "
-					+ ((System.currentTimeMillis() - time) / 1000));
-		}
-		time = System.currentTimeMillis();
-		Collections.sort(list);
-		Collections.sort(ep);
-		if (verbose) {
-			System.out.println("Excecution time for sorting the vectors: "
-					+ ((System.currentTimeMillis() - time) / 1000));
-		}
-		time = System.currentTimeMillis();
-		// Update position of edges
-		Iterator<SuffixVector> iterator = list.iterator();
-		SuffixVector tmp = iterator.next();
-
-		int offset = 0;
-		for (EdgePosition p : ep) {
-			while (tmp != null && tmp.getLocation() <= p.getPosition()) {
-				offset += tmp.getSize();
-				if (iterator.hasNext()) {
-					tmp = iterator.next();
-				} else {
-					tmp = null;
-				}
-			}
-			p.setMovedPosition(p.getMovedPosition() + offset);
-		}
-		if (verbose) {
-			System.out.println("Excecution time for updating the positions: "
-					+ ((System.currentTimeMillis() - time) / 1000));
-		}
-		time = System.currentTimeMillis();
-		BinaryWriter out = new BinaryWriter(outputFile, input);
-		if (!blocks) {
-			out.writeAll(list, ep);
-		} else {
-			out.writeBlocks(list, ep);
-		}
-		if (verbose) {
-			System.out.println("Excecution time for output: "
-					+ ((System.currentTimeMillis() - time) / 1000));
-		}
-		if (verbose) {
-			System.out.println("Total excecution time: "
-					+ ((System.currentTimeMillis() - generalTime) / 1000));
-		}
-		if (evaluate) {
-			BinaryParser p = new BinaryParser(outputFile);
-			if (!blocks) {
-				System.out.println(p.getText());
-			} else {
-				System.out.println(p.getTextWithBlocks());
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Options options = new Options();
+        options.addOption("i", true,
+                "Defines the input file. Define either an input file or a string.");
+        options.addOption("o", true,
+                "Defines the output file. \"default.vc\" if no file is given.");
+        options.addOption("s", true,
+                "Defines the String used for input. Define either a string or an input file.");
+        options.addOption("e", false,
+                "Runs an evaluation of the generated file");
+        options.addOption("m", false,
+                "Generates the SuffixVectors in memory");
+        options.addOption("h", false, "Prints help");
+        options.addOption("help", false, "Prints this help message");
+        options.addOption("v", false, "Verbose");
+        options.addOption("b", false,
+                "Split in blocks. Default turned off");
+        options.addOption("enc", false, "Turns on block encryption");
+        CommandLineParser parser = new GnuParser();
+        // Define a series of variables to check command line options
+        String outputFile = null;
+        boolean evaluate = false;
+        boolean blocks = false;
+        String input = null;
+        boolean inMemory = false;
+        boolean verbose = false;
+        boolean enc = false;
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            if (cmd.hasOption("i")) {
+                File inputFile = new File(cmd.getOptionValue("i"));
+                BufferedReader r = new BufferedReader(new FileReader(inputFile));
+                input = r.readLine();
+            } else if (cmd.hasOption("s")) {
+                input = cmd.getOptionValue("s");
+            } else {
+                throw new MissingOptionException(
+                        "You must define an input, either a string or a file");
+            }
+            if (cmd.hasOption("o")) {
+                outputFile = cmd.getOptionValue("o");
+            } else {
+                outputFile = "default.vc";
+            }
+            if (cmd.hasOption("b")) {
+                blocks = true;
+                if (cmd.hasOption("enc")) {
+                    enc = true;
+                }
+            }
+            if (cmd.hasOption("e")) {
+                evaluate = true;
+            }
+            if (cmd.hasOption("m")) {
+                inMemory = true;
+            }
+            if (cmd.hasOption("v")) {
+                verbose = true;
+            }
+            if (cmd.hasOption("h") || cmd.hasOption("help")) {
+                HelpFormatter h = new HelpFormatter();
+                h.printHelp("Usage: main [options]", options);
+                System.exit(0);
+            }
+        } catch (ParseException e) {
+            System.out.println("Can't parse options: " + e.getMessage());
+            System.exit(0);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // s = "ananasan$";
+        // s = "aatttatttatta$";
+        double generalTime = System.currentTimeMillis();
+        double time = System.currentTimeMillis();
+        CDWAG t = new CDWAG(input);
+        if (verbose) {
+            System.out.println("Excecution time for generating the graph: "
+                    + ((System.currentTimeMillis() - time) / 1000));
+        }
+        time = System.currentTimeMillis();
+        ArrayList<SuffixVector> list = new ArrayList<SuffixVector>();
+        ArrayList<EdgePosition> ep = new ArrayList<EdgePosition>();
+        if (inMemory) {
+            InMemoryVG generator = new InMemoryVG(t);
+            list = generator.getListOfVectors();
+            ep = generator.getListOfEdges();
+            t = null;
+        } else {
+            OutOfMemoryVG generator = new OutOfMemoryVG(t);
+            File vectorFile = new File("_vector.tmp");
+            try {
+                generator.printListOfVectors(vectorFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            t = null;
+            try {
+                ObjectInputStream o = new ObjectInputStream(
+                        new FileInputStream("_vector.tmp"));
+                Object obj = null;
+                while (!((obj = o.readObject()) instanceof String)) {
+                    list.add((SuffixVector) obj);
+                }
+                o.close();
+                vectorFile.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            for (SuffixVector v : list) {
+                for (EdgePosition e : v.getMap().values()) {
+                    ep.add(e);
+                }
+            }
+        }
+        if (verbose) {
+            System.out.println("Excecution time for generating the vectors: "
+                    + ((System.currentTimeMillis() - time) / 1000));
+        }
+        time = System.currentTimeMillis();
+        Collections.sort(list);
+        Collections.sort(ep);
+        if (verbose) {
+            System.out.println("Excecution time for sorting the vectors: "
+                    + ((System.currentTimeMillis() - time) / 1000));
+        }
+        time = System.currentTimeMillis();
+        // Update position of edges
+        Iterator<SuffixVector> iterator = list.iterator();
+        SuffixVector tmp = iterator.next();
+        int offset = 0;
+        for (EdgePosition p : ep) {
+            while (tmp != null && tmp.getLocation() <= p.getPosition()) {
+                offset += tmp.getSize();
+                if (iterator.hasNext()) {
+                    tmp = iterator.next();
+                } else {
+                    tmp = null;
+                }
+            }
+            p.setMovedPosition(p.getMovedPosition() + offset);
+        }
+        if (verbose) {
+            System.out.println("Excecution time for updating the positions: "
+                    + ((System.currentTimeMillis() - time) / 1000));
+        }
+        time = System.currentTimeMillis();
+        BinaryWriter out = new BinaryWriter(outputFile, input);
+        if (!blocks) {
+            out.writeAll(list, ep);
+        } else {
+            out.writeBlocks(list, ep, enc);
+        }
+        if (verbose) {
+            System.out.println("Excecution time for output: "
+                    + ((System.currentTimeMillis() - time) / 1000));
+        }
+        if (verbose) {
+            System.out.println("Total excecution time: "
+                    + ((System.currentTimeMillis() - generalTime) / 1000));
+        }
+        if (evaluate) {
+            BinaryParser p = new BinaryParser(outputFile);
+            if (!blocks) {
+                System.out.println(p.getText());
+            } else {
+                System.out.println(p.getTextWithBlocks());
+            }
+        }
+    }
 }
