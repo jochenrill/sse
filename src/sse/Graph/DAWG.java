2e653c38f7a8a99d8e3140a1aa62f248ed64627c
Jochen Rill <rill@ira.uka.de>
2012-02-03 15:10:58 +0100

    added dawg.java ..

---
+++ src/sse/Graph/DAWG.java
@@ -0,0 +1,254 @@
+package sse.Graph;
+
+import java.io.BufferedWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * This class represents and constructs the DWAG (also called
+ * "compact suffix automaton") for a given text. The algorithm used runs in
+ * O(n).
+ * 
+ * @author Jochen Rill
+ * 
+ */
+public class DAWG implements Iterable<Node> {
+	/**
+	 * Contains the text the graph was constructed for.
+	 */
+	public String text;
+	/**
+	 * Contains the root node.
+	 */
+	public Node source;
+	/**
+	 * Contains the final node.
+	 */
+	public Node bottom;
+	public long nodeCount = 0;
+	public Node sink;
+
+	private LinkedList<Node> nodeList;
+
+	/**
+	 * Initializes and constructs the graph for a given text.
+	 * 
+	 * The constructor initializes the needed variables and constructs the
+	 * graph.
+	 * 
+	 * @param text
+	 *            The text the graph will be constructed for.
+	 */
+	public DAWG(String text) {
+		this.text = text;
+		this.nodeList = new LinkedList<Node>();
+		// creates three start node the algorithm has to work with
+		this.source = new Node(nodeCount++);
+		for (int i = -1; i < text.length(); i++) {
+			source.addPlace(i);
+		}
+		nodeList.add(source);
+		sink = source;
+		update();
+	}
+
+	/**
+	 * Write the graph to a file.
+	 * 
+	 * This method prints the graph to a file in "dot representation". It can be
+	 * viewed with "dotty" in the graphviz package. This only works for graphs
+	 * with reasonable sizes.
+	 * 
+	 * @param fileName
+	 *            The name of the file the graph will be printed to.
+	 */
+	public void printToFile(String fileName) {
+		try {
+			BufferedWriter w = new BufferedWriter(new FileWriter(fileName));
+			w.write("/* this is a generated dot file: www.graphviz.org */\n"
+					+ "digraph suffixtree {\n"
+					+ "\trankdir=LR\nnode[shape=box]");
+			printDot(source, w, new LinkedList<Node>());
+			w.write("}");
+			w.close();
+		} catch (IOException e) {
+			System.out.println("File not found");
+		}
+	}
+
+	// Recursive method to traverse the tree
+	private void printDot(Node root, BufferedWriter writer,
+			LinkedList<Node> visited) throws IOException {
+		if (root.getEdges() != null) {
+			for (Edge e : root.getEdges()) {
+				String string = root.getId() + "->" + e.getEnd().getId() + "\n";
+				writer.write(string);
+
+				writer.write("[" + "label=\"" + e.getEdgeLabel() + "["
+						+ e.getEdgeLabel() + "]" + "\"];\n");
+
+				if (!visited.contains(e.getEnd())) {
+					printDot(e.getEnd(), writer, visited);
+				}
+				visited.add(e.getEnd());
+			}
+		}
+	}
+
+	private void update() {
+
+		for (int i = 0; i < text.length(); i++) {
+			char c = text.charAt(i);
+
+			Node newSink = new Node(nodeCount++);
+			nodeList.add(newSink);
+
+			Edge primEdge = new Edge(c, sink, newSink);
+			sink.addEdge(primEdge);
+
+			for (Integer z : sink.getPlaces()) {
+				if ((z + 1) < text.length() && text.charAt(z + 1) == c) {
+					newSink.addPlace(z + 1);
+				}
+
+			}
+			Node w = sink.getSuffixLink();
+
+			while (w != null && w.getEdge(c) == null) {
+				Edge secEdge = new Edge(c, w, newSink);
+				secEdge.setPrimary(false);
+				w.addEdge(secEdge);
+				w = w.getSuffixLink();
+			}
+
+			Node v = null;
+			if (w != null) {
+				Edge tmpE = w.getEdge(c);
+				if (tmpE != null) {
+					v = tmpE.getEnd();
+				}
+			}
+
+			if (w == null) {
+				newSink.setSuffixLink(source);
+			} else if (w.getEdge(v) != null && w.getEdge(v).isPrimary()) {
+				newSink.setSuffixLink(v);
+			} else {
+				Node newNode = new Node(nodeCount++);
+
+				nodeList.add(newNode);
+				if (v != null) {
+					for (Edge e : v.getEdges()) {
+						Edge secEdge = new Edge(e.getEdgeLabel(), newNode,
+								e.getEnd());
+						secEdge.setPrimary(false);
+						newNode.addEdge(secEdge);
+					}
+				}
+				Edge tmpEdge = w.getEdge(v);
+				Edge primEdge2 = new Edge(tmpEdge.getEdgeLabel(), w, newNode);
+				w.addEdge(primEdge2);
+				w.removeEdge(tmpEdge);
+
+				for (Integer z : w.getPlaces()) {
+					if ((z + 1) < text.length()
+							&& text.charAt(z + 1) == primEdge2.getEdgeLabel()) {
+						newNode.addPlace(z + 1);
+					}
+
+				}
+
+				newSink.setSuffixLink(newNode);
+				newNode.setSuffixLink(v.getSuffixLink());
+				v.setSuffixLink(newNode);
+				w = w.getSuffixLink();
+
+				while (w != null && w.getEdge(c) != null
+						&& w.getEdge(c).getEnd() == v
+						&& !w.getEdge(c).isPrimary()) {
+					Edge tEdge = w.getEdge(c);
+					Edge nEdge = new Edge(tEdge.getEdgeLabel(),
+							tEdge.getStart(), newNode);
+					nEdge.getStart().addEdge(nEdge);
+					nEdge.setPrimary(false);
+					nEdge.getStart().removeEdge(tEdge);
+					w = w.getSuffixLink();
+
+				}
+
+			}
+
+			sink = newSink;
+		}
+	}
+
+	public void storeUniquePaths(boolean paths) {
+
+		/*
+		 * It seems to be a bad idea to count every path in the DAWG because the
+		 * graph is much bigger than the CDAWG. Fortunately we have an
+		 * alternative method of updating the equivalence classes while creating
+		 * the graph
+		 */
+		if (paths) {
+			for (Node n : nodeList) {
+				if (n != sink && n != source) {
+					n.setNumOccurs((findPlace(n)));
+				}
+			}
+		} else {
+
+			for (Node n : nodeList) {
+				if (n == source) {
+					n.setNumOccurs(0);
+				} else if (n == sink) {
+					n.setNumOccurs(1);
+				} else {
+					n.setNumOccurs(n.getPlaces().size());
+				}
+			}
+		}
+	}
+
+	private int findPlace(Node n) {
+		ArrayList<Integer> places = new ArrayList<Integer>();
+		findPlace(n, places);
+
+		if (places.size() == 0) {
+			throw new IllegalStateException("No place found for Node " + n);
+		}
+
+		return places.size();
+
+	}
+
+	private void findPlace(Node n, ArrayList<Integer> places) {
+		for (Edge e : n.getEdges()) {
+
+			while (e.getEnd().getEdges().size() == 1) {
+				e = e.getEnd().getEdges().getFirst();
+			}
+			if (e.getEnd() == sink) {
+				places.add(1);
+			} else {
+				findPlace(e.getEnd(), places);
+			}
+
+		}
+	}
+
+	@Override
+	public Iterator<Node> iterator() {
+		return toList().iterator();
+	}
+
+	@SuppressWarnings("unchecked")
+	public LinkedList<Node> toList() {
+		return (LinkedList<Node>) nodeList.clone();
+	}
+
+}
